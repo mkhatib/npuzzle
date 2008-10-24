14b33f3cc190cb36e02cfc20d04582cf6f8839bf
n.salhi.74 <n.salhi.74@113e92e0-a1c6-11dd-988f-d3ef9af6d102>
2008-10-24 15:19:01 +0000

    message

--- trunk/Main.java
+++ trunk/Main.java
@@ -24,6 +24,15 @@ public final class Main {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello Noura :-)");
+		System.out.println("Hello Mohammad :-)");
+		System.out.println("Enter the intial state:");
+	
+		
+		// we need to check that the total number of tiles is entered, => 9 tiles , 
+		// also, we need to check the range , 0--8 , 
+		// how to save the entered state
+		// and aslo is it a valid state?
+		
 		
 	}
 }
