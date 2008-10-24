1874604ecbee372b15d7f5ff5dfba834c5efa565
mkhatib727 <mkhatib727@113e92e0-a1c6-11dd-988f-d3ef9af6d102>
2008-10-24 12:30:54 +0000

    Added Main.java file, compiled Main.class, and run.bat to run in windows

---
+++ trunk/Main.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * The Main Class
+ *
+ * @author Mohammad Khatib &lt;&gt;
+ * @version $Rev$
+ */
+public final class Main {
+
+    
+	// {{{ untitled constructor
+    /**
+     * 
+     */
+    public Main() {
+        
+    }
+	// }}}
+	
+	// main method
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello Noura :-)");
+		
+	}
+}
